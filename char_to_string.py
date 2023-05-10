234cb4800e9fb67103de3cf92a5de289fea59fde
deepakkumar.maurya <deepakkumarmaurya@fireeye.com>
2023-05-10 11:39:28 +0530

    comi4

---
+++ char_to_string.py
@@ -0,0 +1,1 @@
+h = ['a','b','c','d','e','f']# convert charecter into string# g =''# for i in h:#     g=g+i# print(g)# 2nd way to  convert charecter into string#print(''.join(h))# make string to charecterst='hello'li=[]for i in st:    li+=[i]print(li)  #o/p ['h', 'e', 'l', 'l', 'o']
