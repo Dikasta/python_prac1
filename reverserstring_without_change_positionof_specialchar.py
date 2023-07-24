1ecb4d6c7b01b48cca9ec17a1f25616bc5ba4c10
Deepak Maurya <75212895+Dikasta@users.noreply.github.com>
2023-07-25 00:08:01 +0530

    Update reverserstring_without_change_positionof_specialchar.py

--- reverserstring_without_change_positionof_specialchar.py
+++ reverserstring_without_change_positionof_specialchar.py
@@ -1,1 +1,15 @@
-st='abc/defgh$ij'st1=list(st)j=len(st)-1i=0while (i<j):    if not st1[i].isalpha():        i+=1    elif not st1[j].isalpha():        j-=1    else:        st1[i], st1[j]= st1[j],st1[i]        i+=1        j-=1    s_out=''.join(st1)print(s_out) # op jih/gfedc$ba
+st='abc/defgh$ij'
+st1=list(st)
+j=len(st)-1
+i=0
+while (i<j):
+    if not st1[i].isalpha():
+        i+=1
+    elif not st1[j].isalpha():
+        j-=1
+    else:
+        st1[i], st1[j]= st1[j],st1[i]
+        i+=1
+        j-=1
+    s_out=''.join(st1)
+print(s_out) # op jih/gfedc$ba
