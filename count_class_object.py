31e82a3d2e02beec0701a8d14d4d2cccaf0b9487
Deepak Maurya <75212895+Dikasta@users.noreply.github.com>
2023-11-07 17:36:01 +0530

    Update count_class_object.py

--- count_class_object.py
+++ count_class_object.py
@@ -1,1 +1,17 @@
-# class obj:#     count=0#     def __init__(self, id, name):#             self.id=id#             self.name =name#             obj.count+=1#             print(self.id, self.name)# o1=obj(1,'a')# o2 = obj(2,'b')# o3 = obj(3,'c')# print("call object",obj.count)'''swapcase() :- This function is used to swap the cases of string i.e upper case is converted to lower case and vice versa.str3 = str.swapcase();print (" The swap case converted string is : " + str3) #The swap case converted string is : gEEKSfORgEEKS IS FoR gEEkS'''
+# class obj:
+#     count=0
+#     def __init__(self, id, name):
+#             self.id=id
+#             self.name =name
+#             obj.count+=1
+#             print(self.id, self.name)
+# o1=obj(1,'a')
+# o2 = obj(2,'b')
+# o3 = obj(3,'c')
+# print("call object",obj.count)
+
+'''
+swapcase() :- This function is used to swap the cases of string i.e upper case is converted to lower case and vice versa.
+str3 = str.swapcase();
+print (" The swap case converted string is : " + str3) #The swap case converted string is : gEEKSfORgEEKS IS FoR gEEkS
+'''
