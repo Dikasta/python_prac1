f790eefbc586229cb8f26d0f9d86aefa2f39bcfb
Deepak Maurya <75212895+Dikasta@users.noreply.github.com>
2023-11-07 17:33:52 +0530

    Update dictionary convert into listandtuple form.py

--- dictionary convert into listandtuple form.py
+++ dictionary convert into listandtuple form.py
@@ -1,1 +1,16 @@
-def fun(var,lst):    ls =[]    for k,v in var.items():        if isinstance(v,dict):            lst.append([k, fun(v,ls)])        else:            lst.append((k,v))    return lstvar = {'A': 1, 'B': 2, 'C': {'a': {'p': 'vishal', 'q': {'f':123,'h':32541}}, 'b': 10}}lst =[]print(fun(var,lst))#[('A', 1), ('B', 2), ['C', ['a', [('p', 'vishal'), ('q', 'gupta')], ('b', 10)]]][('A', 1), ('B', 2), ['C', [['a', [('p', 'vishal'), ['q', [('f', 123), ('h', 32541)]]]], ('b', 10)]]]
+
+def fun(var,lst):
+    ls =[]
+    for k,v in var.items():
+        if isinstance(v,dict):
+            lst.append([k, fun(v,ls)])
+
+        else:
+            lst.append((k,v))
+    return lst
+
+var = {'A': 1, 'B': 2, 'C': {'a': {'p': 'vishal', 'q': {'f':123,'h':32541}}, 'b': 10}}
+lst =[]
+print(fun(var,lst))
+#[('A', 1), ('B', 2), ['C', ['a', [('p', 'vishal'), ('q', 'gupta')], ('b', 10)]]]
+[('A', 1), ('B', 2), ['C', [['a', [('p', 'vishal'), ['q', [('f', 123), ('h', 32541)]]]], ('b', 10)]]]
