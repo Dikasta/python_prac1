82f0b9a85da6bab69509c122a0fde755da46fd65
Deepak Maurya <75212895+Dikasta@users.noreply.github.com>
2026-04-29 10:41:45 +0530

    Update add_pair_sum.py

--- add_pair_sum.py
+++ add_pair_sum.py
@@ -9,3 +9,25 @@ def find_pairs(lst, K):
 lst = [1, 5, 3, 7, 9]
 K = 12
 print(find_pairs(lst, K))
+
+
+#flattern dictionary 
+def fun(var, key='', sep='_'):
+    di ={}
+    for k,v in var.items():
+        keys = f'{key}{sep}{k}' if key else k
+        if isinstance(v, dict):
+
+            di.update(fun(v, keys, sep))
+        else:
+            di[keys] = v
+
+    return di
+
+    #print(var)
+
+#o/p {'a': 1, 'b_x': 2, 'b_y_z': 3,  'c_m': 4}
+var=  {
+    'a': 1,
+    'b': {'x': 2, 'y': {'z': 3}},
+    'c': {'m': 4}}
