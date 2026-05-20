f78ded7547751d4f1decd1c3b4f36ddad0a64b08
Deepak Maurya <75212895+Dikasta@users.noreply.github.com>
2026-05-20 12:20:41 +0530

    Update count_class_object.py

--- count_class_object.py
+++ count_class_object.py
@@ -15,3 +15,31 @@ swapcase() :- This function is used to swap the cases of string i.e upper case i
 str3 = str.swapcase();
 print (" The swap case converted string is : " + str3) #The swap case converted string is : gEEKSfORgEEKS IS FoR gEEkS
 '''
+
+
+#======================
+# delete class
+class A:
+    def f(self, x):
+        print("jhfgwjrh", x)
+
+class B(A):
+    def fun(self):
+        print("jjhgfsdjhh")
+del A
+o = B()
+print(o.f(4)) # call f (function) with class B object then it will work fine because B already inherit A class property,
+#while class A already call with del function before B object creation.
+o1 = A() # here through error because class A is deleted
+
+
+
+# delete class method
+class MyClass:
+    def my_function(self):
+        print("Hello")
+
+# Delete the function from the class
+del MyClass.my_function
+o = MyClass()
+print(o.my_function()) # through Attribute error because myfuntion already deleted
